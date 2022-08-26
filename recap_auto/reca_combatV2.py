81ec2b22d9409541ae5763e7595913156ab27b91
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2022-08-26 20:49:47 +0200

    prog

--- recap_auto/reca_combatV2.py
+++ recap_auto/reca_combatV2.py
@@ -17,7 +17,20 @@ if __name__=='__main__':
     PA_DEBUT = data['Combat'][5][4]
     COMBO_EP_DEBUT = data['Combat'][21][1]
     COMBO_ES_DEBUT = data['Combat'][22][1]
-
+    MAINTENU_SOMME = data['Combat'][11][1]
+    NEGATIF_SOMME_DEBUT = data['Combat'][12][1]
+    POSITIF_SOMME_DEBUT = data['Combat'][13][1]
+    TOTAL_DEFENDU = 0
+    TOTAL_SUBI = 0
+    NEGATIF_SOMME_FIN = 0
+    POSITIF_SOMME_FIN = 0
+    PV_FIN = 0
+    EP_FIN = 0
+    ES_FIN = 0
+    PA_FIN = 0
+    PA_BONUS = 0
+    COMBO_EP_FIN = 0
+    COMBO_ES_FIN = 0
 
     print('[hide][spoiler="Tour '+ str(TOUR) + '"][h3][color=#929291]Stats de[/color] début de tour[/h3]')
     print('\n')
@@ -32,37 +45,37 @@ if __name__=='__main__':
     print('\n')
     print('[h3][color=#929291]Phase [color=#344b99]Défensive[/color][/color][/h3]')
     print('\n')
-    # print('[u][b][color=#a2783c]Récapitulatif :[/color][/b][/u][list]')
-    # print('[*][b]Maintenu :[/b] '+MAINTENU_SOMME)
-    # print('[*][b]Valeurs négatives :[/b] '+NEGATIF_SOMME_DEBUT)
-    # print('[*][b]Valeurs positives :[/b] '+POSITIF_SOMME_DEBUT+'')
-    # print('[/list][u][b][color=#a2783c]Techniques défensives :[/color][/b][/u][list]')
+    print('[u][b][color=#a2783c]Récapitulatif :[/color][/b][/u][list]')
+    print('[*][b]Maintenu :[/b] ' + str(MAINTENU_SOMME))
+    print('[*][b]Valeurs négatives :[/b] ' + str(NEGATIF_SOMME_DEBUT))
+    print('[*][b]Valeurs positives :[/b] ' + str(POSITIF_SOMME_DEBUT))
+    print('[/list][u][b][color=#a2783c]Techniques défensives :[/color][/b][/u][list]')
     # print('[*][b][Niveau X] Nom[/b]')
     # print('[Branche principale] [Branche secondaire] [Type]')
     # print('[u]Effets :[/u]')
     # print('[u]Dépense :[/u]')
     # print('[spoiler="Descriptif RP"]Description[/spoiler]')
-    # print('[/list][u][b][color=#a2783c]Résumé phase défensive :[/color][/b][/u]')
-    # print('[list][*][b]Défendu :[/b] '+TOTAL_DEFENDU)
-    # print('[*][b]Subi :[/b] '+TOTAL_SUBI+' [/list]')
-    # print('[h3][color=#929291]Phase [color=#a23d3c]Offensive[/color][/color][/h3]')
-    # print('\n')
-    # print('[u][b][color=#a2783c]Techniques offensives :[/color][/b][/u][list]')
+    print('[/list][u][b][color=#a2783c]Résumé phase défensive :[/color][/b][/u]')
+    print('[list][*][b]Défendu :[/b] ' + str(TOTAL_DEFENDU))
+    print('[*][b]Subi :[/b] ' + str(TOTAL_SUBI) + ' [/list]')
+    print('[h3][color=#929291]Phase [color=#a23d3c]Offensive[/color][/color][/h3]')
+    print('\n')
+    print('[u][b][color=#a2783c]Techniques offensives :[/color][/b][/u][list]')
     # print('[*][b][Niveau X] Nom[/b]')
     # print('[Branche principale] [Branche secondaire] [Type]')
     # print('[u]Effets :[/u]')
     # print('[u]Dépense :[/u]')
     # print('[spoiler="Descriptif RP"]Description[/spoiler]')
-    # print('[/list][u][b][color=#a2783c]Récapitulatif :[/color][/b][/u]')
-    # print('[list][*][b]Valeurs négatives :[/b] '+NEGATIF_SOMME_FIN)
-    # print('[*][b]Valeurs positives :[/b] '+POSITIF_SOMME_FIN+'[/list]')
-    # print('[h3][color=#929291]Stats de[/color] fin de tour[/h3]')
-    # print('\n')
-    # print('[b][color=#92d050]Santé : [color=#f9f9f9]'+PV_FIN+'[/color]/'+PV_TOTAL+'[/color][/b]')
-    # print('[b][color=#c83737]Énergie Physique : [color=#f9f9f9]'+EP_FIN+'[/color]/'+EP_TOTAL+'[/color][/b]')
-    # print('[b][color=#2a4cbc]Énergie Spirituelle : [color=#f9f9f9]'+ES_FIN+'[/color]/'+ES_TOTAL+'[/color][/b]')
-    # print('\n')
-    # print("[b][color=#783da2]Points d'Action : [color=#f9f9f9]X[/color]/X[/color][/b] (+0 PA au prochain tour)")
-    # print('\n')
-    # print('[b][color=#c83737]Combo EP : '+COMBO_EP_FIN+'[/color][/b]')
-    # print('[b][color=#2a4cbc]Combo ES : '+COMBO_ES_FIN+'[/color][/b][/spoiler][/hide]')
+    print('[/list][u][b][color=#a2783c]Récapitulatif :[/color][/b][/u]')
+    print('[list][*][b]Valeurs négatives :[/b] ' + str(NEGATIF_SOMME_FIN))
+    print('[*][b]Valeurs positives :[/b] ' + str(POSITIF_SOMME_FIN) + '[/list]')
+    print('[h3][color=#929291]Stats de[/color] fin de tour[/h3]')
+    print('\n')
+    print('[b][color=#92d050]Santé : [color=#f9f9f9]' + str(PV_FIN) + '[/color]/' + str(PV_TOTAL) + '[/color][/b]')
+    print('[b][color=#c83737]Énergie Physique : [color=#f9f9f9]' + str(EP_FIN) + '[/color]/' + str(EP_TOTAL) + '[/color][/b]')
+    print('[b][color=#2a4cbc]Énergie Spirituelle : [color=#f9f9f9]' + str(ES_FIN) + '[/color]/' + str(ES_TOTAL) + '[/color][/b]')
+    print('\n')
+    print("[b][color=#783da2]Points d'Action : [color=#f9f9f9]" + str(PA_FIN) + "[/color]/X[/color][/b] (+" + str(PA_BONUS) + " PA au prochain tour)")
+    print('\n')
+    print('[b][color=#c83737]Combo EP : ' + str(COMBO_EP_FIN) + '[/color][/b]')
+    print('[b][color=#2a4cbc]Combo ES : ' + str(COMBO_ES_FIN) + '[/color][/b][/spoiler][/hide]')
