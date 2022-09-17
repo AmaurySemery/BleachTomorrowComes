80af9b84a4976d4f10b8f06cb28efe893443a5d1
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2022-09-17 15:57:52 +0200

    0

--- Extract_data_BTC/extract_stat_groupes.py
+++ Extract_data_BTC/extract_stat_groupes.py
@@ -2,15 +2,16 @@ import requests
 import requests
 from bs4 import BeautifulSoup
 
-
-
 url = "https://www.before-tomorrow-comes.fr/g3-gotei-13"
 page = requests.get(url)
-# Voir le code html source
-# print(page.content)
 soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup)
-titres = soup.find_all("table", class_="table1")
-print(titres)
-for name in titres:
-    print(name.get('row1'))
+MEMBRES_GOTEI_13 = soup.find_all('strong')
+LISTE_GOTEI_13 = []
+for name in MEMBRES_GOTEI_13:
+    name = str(name)
+    replace_strong_1 = name.replace("<strong>","")
+    replace_strong_2 = replace_strong_1.replace("</strong>","")
+    if replace_strong_2 != "Gotei 13" and replace_strong_2 != "Reiō" and replace_strong_2 != '<a href="https://www.forumactif.com" target="_blank">Créer un forum</a>' and replace_strong_2 != '<a href="https://www.forumactif.com/forum-gratuit" target="_blank">Forum gratuit</a>':
+        LISTE_GOTEI_13.append(replace_strong_2)
+
+print(LISTE_GOTEI_13)
