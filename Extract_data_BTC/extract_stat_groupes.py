ba2c59cb1ad21986d5a7a56371f38585ad0145ed
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2022-09-17 20:24:04 +0200

    Update extract_stat_groupes.py

--- Extract_data_BTC/extract_stat_groupes.py
+++ Extract_data_BTC/extract_stat_groupes.py
@@ -3,9 +3,6 @@ from bs4 import BeautifulSoup
 
 url_gotei_groupe = "https://www.before-tomorrow-comes.fr/g3-gotei-13"
 url_classement = 'https://www.before-tomorrow-comes.fr/memberlist?mode=point&order=DESC&submit=Ok&username'
-resp = http.request('GET', url)
-# print(resp.data.decode('utf-8'))
-# print(resp.data)
 
 page = requests.get(url_gotei_groupe)
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -18,4 +15,18 @@ for name in MEMBRES_GOTEI_13:
     if replace_strong_2 != "Gotei 13" and replace_strong_2 != "Reiō" and replace_strong_2 != '<a href="https://www.forumactif.com" target="_blank">Créer un forum</a>' and replace_strong_2 != '<a href="https://www.forumactif.com/forum-gratuit" target="_blank">Forum gratuit</a>':
         LISTE_GOTEI_13.append(replace_strong_2)
 
-print(LISTE_GOTEI_13)
+# print(LISTE_GOTEI_13)
+
+page = requests.get(url_classement)
+soup = BeautifulSoup(page.content, 'html.parser')
+# print(soup)
+CLASSEMENT = soup.find_all("div", class_='forumbg forumbg-table')
+print(CLASSEMENT[0])
+
+for name in CLASSEMENT:
+    name = str(name)
+    replace_strong_1 = name.replace("<strong>","")
+    replace_strong_2 = replace_strong_1.replace("</strong>","")
+    # print(replace_strong_2)
+    # if replace_strong_2 != "Gotei 13" and replace_strong_2 != "Reiō" and replace_strong_2 != '<a href="https://www.forumactif.com" target="_blank">Créer un forum</a>' and replace_strong_2 != '<a href="https://www.forumactif.com/forum-gratuit" target="_blank">Forum gratuit</a>':
+    #     LISTE_GOTEI_13.append(replace_strong_2)
