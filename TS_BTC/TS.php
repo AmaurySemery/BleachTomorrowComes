8a1077ef064d857113eea7a8567e98054c4f3aaf
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2022-10-20 21:19:02 +0200

    Update TS.php

--- TS_BTC/TS.php
+++ TS_BTC/TS.php
@@ -1,24 +1,42 @@
+<!DOCTYPE html>
 <html>
-    <body>
-        <table>
-            <?php    
-                shell_exec ('/var/www/html/');
-                shell_exec ('python3 TS_script.py');
-                usleep(5000000);
-                $fp = fopen ( "/var/www/html/affichage.csv" , "r" );
-                while (( $data = fgetcsv ( $fp , 1000 , "," )) !== FALSE ) {
-                $i = 0;
-                echo "<tr>";
-                foreach($data as $row) {
-                echo "<td>" . $row . "</td>";
-                $i++ ;
-                }
-                echo "/<tr>";
-                }
-                fclose ( $fp );
-            ?>
-        </table>
-        <style> iframe{display: none;} </style>        
-        <iframe name="frame"></iframe> 
+      
+<head>
+    <title>
+        TS Before Tomorrow Comes
+    </title>
+</head>
+  
+<body>
+    <form method="post">
+        <input type="submit" name="button1"
+                value="Mouliner le programme"/>
+          
+        <input type="submit" name="button2"
+                value="Sortir le classement"/>
+    </form>
+    <?php
+      
+        if(isset($_POST['button1'])) {
+            $Commande = shell_exec("cd /var/www/html && python3 TS_script.py");
+            echo "$Commande";
+        }
+        if(isset($_POST['button2'])) {
+            usleep(5000000);
+            $fp = fopen ( "/var/www/html/affichage.csv" , "r" );
+            while (( $data = fgetcsv ( $fp , 1000 , "," )) !== FALSE ) {
+            $i = 0;
+            echo "<tr>";
+            foreach($data as $row) {
+            echo "<div>" . $row . "</div>";
+            $i++ ;
+            }
+            echo "<tr>";
+            }
+            fclose ( $fp );
+        }
+    ?>
+      
+    
     </body>
 </html>
