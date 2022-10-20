7264c00ce8115f9989c2bd23d49c75162183612a
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2022-10-20 20:13:33 +0200

    Update TS.php

--- TS_BTC/TS.php
+++ TS_BTC/TS.php
@@ -1,24 +1,24 @@
 <html>
     <body>
-    <table>
-
-<?php
-             shell_exec ('/var/www/html/');
-             shell_exec ('python3 TS_script.py');
-             echo "Waiting for 5 seconds...<br/>";
-             usleep(5000000);
-             $fp = fopen ( "/var/www/html/affichage.csv" , "r" );
-             while (( $data = fgetcsv ( $fp , 1000 , "," )) !== FALSE ) {
-             $i = 0;
-             echo "<tr>";
-             foreach($data as $row) {
-             echo "<td>" . $row . "</td>";
-             $i++ ;
-             }
-             echo "/<tr>";
-             }
-             fclose ( $fp );
-             ?>
-             </table>
-</body>
+        <table>
+            <?php    
+                shell_exec ('/var/www/html/');
+                shell_exec ('python3 TS_script.py');
+                usleep(5000000);
+                $fp = fopen ( "/var/www/html/affichage.csv" , "r" );
+                while (( $data = fgetcsv ( $fp , 1000 , "," )) !== FALSE ) {
+                $i = 0;
+                echo "<tr>";
+                foreach($data as $row) {
+                echo "<td>" . $row . "</td>";
+                $i++ ;
+                }
+                echo "/<tr>";
+                }
+                fclose ( $fp );
+            ?>
+        </table>
+        <style> iframe{display: none;} </style>        
+        <iframe name="frame"></iframe> 
+    </body>
 </html>
