fae2382cdfc4f46a4b092f2f42f4e2d23a040760
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2022-10-20 18:44:31 +0200

    Update TS.php

--- TS_BTC/TS.php
+++ TS_BTC/TS.php
@@ -3,7 +3,11 @@
     <table>
 
 <?php
-             $fp = fopen ( "/var/www/html/classement_BTC.csv" , "r" );
+             shell_exec ('/var/www/html/');
+             shell_exec ('python3 TS_script.py');
+             echo "Waiting for 5 seconds...<br/>";
+             usleep(5000000);
+             $fp = fopen ( "/var/www/html/affichage.csv" , "r" );
              while (( $data = fgetcsv ( $fp , 1000 , "," )) !== FALSE ) {
              $i = 0;
              echo "<tr>";
